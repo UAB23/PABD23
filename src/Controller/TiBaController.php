5035ff1e65073dc8455075ce16ff121d6d5518af
tiberiu.bartha@gmail.com <tiberiu.bartha@gmail.com>
2023-04-25 18:04:03 +0300

    TiBa: adaugare fis necesare pt pag principala

---
+++ src/Controller/TiBaController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class TiBaController extends AbstractController
+{
+    #[Route('/TiBa')]
+    public function TiBaController()
+    {
+        return $this->render('TiBa.html.twig');
+    }
+}

c1d14d93b83d1fd795a450185077cd08d57be460
unknown <dobrinpaul99@gmail.com>
2023-05-21 22:54:13 +0300

    Implementare pagina personala

---
+++ src/Controller/DobrinPaulController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DobrinPaulController extends AbstractController
+{
+    #[Route('/paul')]
+    public function paul()
+    {
+        return $this->render('paul.html.twig');
+    }
+}

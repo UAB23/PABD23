110e7c467c419ec4c8bb6566e17b17c81f77633b
PARAIPAN SORIN <paraipan.sorin1993@gmail.com>
2023-06-19 02:08:06 +0300

    Add Dynamic Page

---
+++ src/Controller/TodoController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Todo;
+use App\Form\TodoType;
+use App\Repository\TodoRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/todo')]
+class TodoController extends AbstractController
+{
+    #[Route('/', name: 'app_todo_index', methods: ['GET'])]
+    public function index(TodoRepository $todoRepository): Response
+    {
+        return $this->render('todo/index.html.twig', [
+            'todos' => $todoRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_todo_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, TodoRepository $todoRepository): Response
+    {
+        $todo = new Todo();
+        $form = $this->createForm(TodoType::class, $todo);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $todoRepository->save($todo, true);
+
+            return $this->redirectToRoute('app_todo_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('todo/new.html.twig', [
+            'todo' => $todo,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_todo_show', methods: ['GET'])]
+    public function show(Todo $todo): Response
+    {
+        return $this->render('todo/show.html.twig', [
+            'todo' => $todo,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_todo_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Todo $todo, TodoRepository $todoRepository): Response
+    {
+        $form = $this->createForm(TodoType::class, $todo);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $todoRepository->save($todo, true);
+
+            return $this->redirectToRoute('app_todo_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('todo/edit.html.twig', [
+            'todo' => $todo,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_todo_delete', methods: ['POST'])]
+    public function delete(Request $request, Todo $todo, TodoRepository $todoRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$todo->getId(), $request->request->get('_token'))) {
+            $todoRepository->remove($todo, true);
+        }
+
+        return $this->redirectToRoute('app_todo_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Repository/TodoRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Todo;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Todo>
+ *
+ * @method Todo|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Todo|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Todo[]    findAll()
+ * @method Todo[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class TodoRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Todo::class);
+    }
+
+    public function save(Todo $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Todo $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return Todo[] Returns an array of Todo objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('t')
+//            ->andWhere('t.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('t.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Todo
+//    {
+//        return $this->createQueryBuilder('t')
+//            ->andWhere('t.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
